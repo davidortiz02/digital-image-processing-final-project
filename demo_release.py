02d9875b61b4b90129bb172e3bc1f8c69993d60e
ChinamanRyan <dudeizawsom@gmail.com>
2026-05-07 20:13:02 -0500

    Fixed color of resnet, looks decent but not as good as the others

--- demo_release.py
+++ demo_release.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from colorizers import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i','--img_path', type=str, default='imgs/khoaluu.jpg')
+parser.add_argument('-i','--img_path', type=str, default='imgs/union.jpg')
 parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
 parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
 opt = parser.parse_args()
@@ -13,25 +13,28 @@ opt = parser.parse_args()
 # load colorizers
 colorizer_eccv16 = eccv16(pretrained=True).eval()
 colorizer_siggraph17 = siggraph17(pretrained=True).eval()
-colorizer_resnet50 = resnet50_colorizer(pretrained=False).eval()
+colorizer_resnet50 = resnet50_colorizer(pretrained=True).eval()
 if(opt.use_gpu):
 	colorizer_eccv16.cuda()
 	colorizer_siggraph17.cuda()
-	colorizer_resnet50.cude()
+	colorizer_resnet50.cuda()
 
 # default size to process images is 256x256
 # grab L channel in both original ("orig") and resized ("rs") resolutions
 img = load_img(opt.img_path)
-(tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(512,512))
+(tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
 if(opt.use_gpu):
 	tens_l_rs = tens_l_rs.cuda()
+
+tens_l_input = tens_l_rs / 50.0 - 1.0  # match training normalization
+out_ab = colorizer_resnet50(tens_l_input).cpu() * 110.0  # denormalize output
 
 # colorizer outputs 256x256 ab map
 # resize and concatenate to original L channel
 img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
 out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
 out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
-out_img_resnet50 = postprocess_tens(tens_l_orig, colorizer_resnet50(tens_l_rs).cpu())
+out_img_resnet50 = postprocess_tens(tens_l_orig, out_ab)
 
 plt.imsave('%s_eccv16.png'%opt.save_prefix, out_img_eccv16)
 plt.imsave('%s_siggraph17.png'%opt.save_prefix, out_img_siggraph17)
